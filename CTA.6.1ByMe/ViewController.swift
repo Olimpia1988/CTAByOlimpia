7ec55dedde4679f0f5c023161bbbdef3b52bdb83
Olimpia <olimpiacompean@prusuit.org>
2019-11-26 13:41:11 -0500

    first commit

---
+++ CTA.6.1ByMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CTA.6.1ByMe
+//
+//  Created by Olimpia on 11/26/19.
+//  Copyright © 2019 Olimpia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
